1fed6a21f9053ef13c1b2c4bc0eec265c55b4978
cemGr <66869029+cemGr@users.noreply.github.com>
2020-09-05 01:19:44 +0200

    fix

--- js/animation.js
+++ js/animation.js
@@ -6,16 +6,36 @@ function myMove() {
   var firstline= "rotate(";
   var lastline="deg)"
   var rotation;
-  //var pos2 = 350;
-  var id = setInterval(frame, 180);
+  var minutes = 1000 * 60;
+  var hours = minutes * 60;
+  var days = hours * 24;
+  var years = days * 365;
+
+
+  var id = setInterval(frame,1000);
   function frame() {
-    if (pos == 360) {
-    } else {
+    if (pos == 60) {
+      pos=0;
+      min+=1;
+      if(min==60){
+        min=0;
+        hour+=1;
+        if(hour==24){
+          hour=0;
+        }else{hour+=1;}
+      }else{min+=1;}
+    }else {
+      var date= new Date();
+      var d = date.getSeconds();
       pos++;
       rotate=firstline+pos+lastline;
       elem.style.transform= rotate;
+      console.log(d);
+      delete date;
       //elem2.style.top = pos2 + "px";
       //elem2.style.left= pos2 + "px";
     }
-  }
+
 };
+
+myMove();
